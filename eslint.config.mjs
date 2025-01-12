126eecfb7892a24eb52adee1caa57707b9292650
n4to4 <n4to4k@gmail.com>
2025-01-12 18:33:19 +0900

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,14 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
 );
